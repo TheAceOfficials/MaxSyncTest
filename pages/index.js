ae5c0b3b4c887902186db52f8e123f69c85937a4
TheAceOfficials <acegamerytofficial@gmail.com>
2025-09-19 15:47:37 +0530

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      flexDirection: "column",
+      backgroundColor: "#0d0d0d",
+      color: "#fff",
+      fontFamily: "Arial"
+    }}>
+      <h1>🚀 Welcome to My Vercel Site</h1>
+      <p>Yeh site abhi successfully deploy hogi!</p>
+    </div>
+  );
+}
